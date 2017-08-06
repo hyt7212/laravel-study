b348f6f450a21e65ea67ecf1b563f8d6d37d3e09
Kerlin Huang <sean0722@msn.com>
2017-08-06 19:35:12 +0800

    显示用户列表；数据填充

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,6 +22,12 @@ class UsersController extends Controller
         ]);
     }
 
+    public function index()
+    {
+        $users = User::all();
+        return view('users/index', compact('users'));
+    }
+
     public function create()
     {
         return view('users/create');
